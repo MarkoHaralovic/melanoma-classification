fa3fb3308aeb6b54c57bc2d7fa5e2fc65b44bf51
MarkoHaralovic <mharalovic11@gmail.com>
2025-04-13 13:17:21 +0200

    Added FocalLoss and DomainDiscriminativeLoss

--- src/models/losses/criterion.py
+++ src/models/losses/criterion.py
@@ -1,7 +1,8 @@
-from torch import nn
+from torch import long, nn
 from torch.nn import functional as F
 import torch
 import numpy as np
+from torch.autograd import Variable
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -94,7 +95,51 @@ class DomainIndependentLoss(nn.Module):
       loss = self.criterion(logits, domain_dependant_targets, weight=self.weight)
       return loss
    
+   
+class DomainDiscriminativeLoss(nn.Module):
+   def __init__(self, weight=None, ):
+      super(DomainDiscriminativeLoss, self).__init__()
+      self.criterion = F.cross_entropy
+      self.weight = weight
       
+   def forward(self, logits, targets):
+      return self.criterion(logits, targets, weight=self.weight)
+   
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)): 
+            self.alpha = torch.Tensor([alpha, 1-alpha])
+        if isinstance(alpha, list): 
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average: 
+            return loss.mean()
+        else: 
+            return loss.sum()
+        
 def labels_to_class_weights(samples, ifw_by_skin_type = False, num_classes=2, alpha=0.5, beta=0.5):
    """
    Calculate class weights based on class frequencies in the dataset, taking into account both
